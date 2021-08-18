aade806f5cb3afeffa0d79559e5ead79dd6abedc
magacula <makoigacula@gmail.com>
2021-08-18 14:36:08 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,14 +8,19 @@ const form = document.querySelector("#search");
 form.addEventListener("submit", onSubmit);
 
 // handles form submit
+// async functions run asynchronously in the background (not blocking the main thread of execution or CALL stack)
 async function onSubmit(e) {
   e.preventDefault();
 
   // retrieve zipcode
   let zip = form.elements[0].value;
-  console.log(zip);
 
-  // Sends GET request to get city, state, and lat & long values
+  /***** FIX #1: Clear text filed so they cant make the same API call  ******/
+  form.reset();
+
+  // Sends GET request to retrieve city, state, and lat & long values
+  // await will stop the code execution at this point of the function, until promise has been
+  // fulfilled (when the data has been fetched)
   let getCoordinates = await getLocation(zip);
 
   let city = getCoordinates.city;
@@ -24,15 +29,15 @@ async function onSubmit(e) {
   let long = getCoordinates.longitude;
 
   let weather = await getWeather(lat, long);
-
+  console.log("weather", weather);
   renderForecast(city, state, weather);
 }
 
-// handles API call to get geolocation via Geolocation API
-async function getLocation(zip) {
+/***** FIX 2: functions fetches data when specifying the URL we want to hit  ******/
+// Function that deals with an abstraction of making more than 1 api call
+async function fetchData(url) {
   try {
-    let geoURL = `https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=${zip}`;
-    let res = await fetch(geoURL);
+    let res = await fetch(url);
     let data = await res.json();
     console.log(data);
     return data;
@@ -41,7 +46,14 @@ async function getLocation(zip) {
   }
 }
 
-// handle API call to get forecast via Forecast API
+// handles API call to fetch geolocation data via Geolocation API
+async function getLocation(zip) {
+  let geoURL = `https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=${zip}`;
+  let getGeoData = await fetchData(geoURL);
+  return getGeoData;
+}
+
+// handle API call to fetch forecast data of each day via Forecast API
 // takes lat & long values as params
 async function getWeather(lat, long) {
   const today = new Date();
@@ -51,15 +63,10 @@ async function getWeather(lat, long) {
     today.getMonth() + 1
   }/${today.getDate()}/${today.getFullYear()}`;
 
-  try {
-    let weatherURL = `https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=-${long}&date=${date}`;
-    let res = await fetch(weatherURL);
-    let data = await res.json();
-
-    return data.daily.data;
-  } catch (error) {
-    console.log(error);
-  }
+  let weatherURL = `https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=${lat}&longitude=-${long}&date=${date}`;
+  let getWeatherData = await fetchData(weatherURL);
+  console.log(getWeatherData);
+  return getWeatherData.daily.data;
 }
 
 // renders out weather forecast UI to DOM
@@ -68,27 +75,51 @@ function renderForecast(city, state, weather) {
   // display heading for forecast of city, state
   cityForecast.innerHTML = `<h2>WEATHER FORECAST FOR ${city.toUpperCase()}, ${state}.</h2>`;
 
-  const date = ["Today", "Tomorrow", "Day After Tomorrow"];
+  /***** FIX #3: Displaying days of the week  *****/
+  // Uses "wrapping effect" on array with modulus operator %
 
-  // constructs forecast items. Iterate only three times since we need to display 3-day forecast
-  for (let i = 0; i < weather.length - 2; i++) {
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  // gets the day of the week: Ex) if Monday, will return 1
+  // we need to subtract 1 to match the indices in the days array
+  let dayOfTheWeekIndex = new Date().getDay() - 1;
+  console.log(dayOfTheWeekIndex);
+
+  // we subtract 2 from the end since we are only getting the 3-day forecast
+  // we can take out if we want to get the 5-day (what the api gives us back)
+  for (
+    let i = dayOfTheWeekIndex;
+    i < weather.length + dayOfTheWeekIndex - 2;
+    i++
+  ) {
     // creates a forecast item for each day
     let li = document.createElement("li");
 
     li.innerHTML = `
-    <h3 id="date">${date[i]}</h3>
+    <h3 id="date">${
+      i === dayOfTheWeekIndex ? "Today" : days[parseInt(i % days.length)]
+    }</h3>
     <div id="weatherInfo">
       <div>
-      <img src="${date[i].icon}" alt="" />
+      <img src="${weather[i - dayOfTheWeekIndex].icon}" alt="" />
       </div>
       <div>
-        <p>${weather[i].icon}</p>
+        <p>${weather[i - dayOfTheWeekIndex].icon}</p>
         <p><span style='font-weight:bold'>${Math.round(
-          weather[i].temperatureHigh
-        )}</span>° / ${Math.round(weather[i].temperatureLow)}°</p>
+          weather[i - dayOfTheWeekIndex].temperatureHigh
+        )}</span>° / ${Math.round(
+      weather[i - dayOfTheWeekIndex].temperatureLow
+    )}°</p>
       </div>
     </div>`;
-
     document.getElementById("forecastList").appendChild(li);
   }
 }
